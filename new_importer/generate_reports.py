1dc22a606d4e60a29bbd839aebc84729bb2fcaf3
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2022-01-10 17:13:20 +0100

    basic "segment" handling for views

--- new_importer/generate_reports.py
+++ new_importer/generate_reports.py
@@ -7,15 +7,44 @@ import mysql.connector
 from config import *
 from statements import *
 
+segments = ['overall', 'downloads', 'repository', 'others', 'services', 'lrt']
+
+
+def _segment2prefix(segment):
+    if segment == 'overall':
+        return ''
+    else:
+        return segment
+
 
 def get_views(cursor):
     # XXX these totals include also numbers from anyone using our footer by accident
 
     start_time = time.perf_counter()
-    cursor.execute(hits_visits_aggregated)
-    yearly_report = {}
-    per_month_report = {}
-    per_day_report = {}
+    for segment in segments:
+        segment_start_time = time.perf_counter()
+        query = _format_query_for_segment(hits_visits_aggregated, segment)
+        prefix = os.path.join('views', _segment2prefix(segment))
+        cursor.execute(query)
+        yearly_report = {}
+        per_month_report = {}
+        per_day_report = {}
+        _process_views_results(cursor, yearly_report, per_month_report, per_day_report)
+        _write_views(prefix, yearly_report, per_month_report, per_day_report)
+        elapsed_time = time.perf_counter() - segment_start_time
+        print("Elapsed time in get_views segment '{}': {}".format(segment, elapsed_time))
+        # download report from overall???
+        # elif row['idsite'] == 4:
+    elapsed_time = time.perf_counter() - start_time
+    print("Elapsed time in get_views: {}".format(elapsed_time))
+
+
+def _format_query_for_segment(query, segment):
+    segment_where_selector = segment2where[segment]
+    return query.format(segment_where_selector)
+
+
+def _process_views_results(cursor, yearly_report, per_month_report, per_day_report):
     for row in cursor:
         # grand total
         if not row['idsite'] and not row['year'] and not row['month'] and not row['day']:
@@ -33,8 +62,8 @@ def get_views(cursor):
         elif not row['idsite'] and not row['day']:
             year = per_month_report.setdefault(row['year'], {})
             year[row['month']] = {
-                    'nb_pageviews': row['hits'],
-                    'nb_uniq_pageviews': row['visits']
+                'nb_pageviews': row['hits'],
+                'nb_uniq_pageviews': row['visits']
             }
         # per day total
         elif not row['idsite']:
@@ -44,9 +73,10 @@ def get_views(cursor):
                 'nb_pageviews': row['hits'],
                 'nb_uniq_pageviews': row['visits']
             }
-        # download report
-        # elif row['idsite'] == 4:
-    output_prefix = os.path.join(output_dir, 'views')
+
+
+def _write_views(prefix, yearly_report, per_month_report, per_day_report):
+    output_prefix = os.path.join(output_dir, prefix)
     print("Writing to {}".format(output_prefix))
     os.makedirs(output_prefix)
     with open(os.path.join(output_prefix, 'response.json'), 'w') as f:
@@ -61,8 +91,6 @@ def get_views(cursor):
             os.makedirs(os.path.dirname(filename), exist_ok=True)
             with open(filename, 'w') as f:
                 json.dump({'response': {year: {month: per_day_report[year][month]}}}, f)
-    elapsed_time = time.perf_counter() - start_time
-    print("Elapsed time in get_views: {}".format(elapsed_time))
 
 
 def main():
--- new_importer/statements.py
+++ new_importer/statements.py
@@ -3,8 +3,24 @@ SELECT count(*) as hits, count( DISTINCT idvisit, idaction) as visits, idsite, Y
 MONTH(server_time) as month, DAY(server_time) as day
     FROM piwik_log_link_visit_action
         LEFT JOIN piwik_log_action ON piwik_log_action.idaction = piwik_log_link_visit_action.idaction_url
-            WHERE idsite IN (2,4)
-                AND type = 1
-                    AND server_time >= '2014-01-01'
+            WHERE type = 1 AND server_time >= '2014-01-01' {}
                         GROUP BY year, month, day, idsite WITH ROLLUP;
 """
+
+segment2where = {
+    'overall': 'AND idsite IN (2,4) ',
+    'downloads': 'AND idsite = 4',
+    'repository': """
+        AND idsite = 2 AND name like 'lindat.mff.cuni.cz/repository%'
+    """,
+    'others': """
+        AND idsite = 2 AND name not like 'lindat.mff.cuni.cz%'
+    """,
+    'services': """
+        AND idsite = 2 AND name like 'lindat.mff.cuni.cz/services%'
+    """,
+    'lrt': """
+        AND idsite = 2 AND name like 'lindat.mff.cuni.cz/repository%LRT%'
+    """
+}
+
