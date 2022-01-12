bfdfd7a312454df4ce998dd00e7715066384b924
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2022-01-12 16:36:29 +0100

    cleanup / static data for new version

--- scripts/luc/__init__.py
+++
@@ -1,4 +0,0 @@
-import lucene
-
-env = lucene.initVM()
-print('lucene', lucene.VERSION)
--- scripts/wsgi.py
+++
@@ -1,5 +0,0 @@
-from search_application import app as application
-
-
-if __name__ == "__main__":
-    application.run(threaded=False)
